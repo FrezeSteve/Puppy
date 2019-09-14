1e62c1955201fa52bc55238ee09d5de412ad4673
frezesteve <stevefrisson@gmail.com>
2019-09-14 08:52:31 +0300

    Added a relationship in the database

--- forms.py
+++ forms.py
@@ -10,3 +10,14 @@ class AddForm(FlaskForm):
 class DelForm(FlaskForm):
     id = IntegerField("Id Number of Puppy to Remove: ")
     submit = SubmitField("Remove Puppy")
+
+
+class AddOwnerForm(FlaskForm):
+    name = StringField('Name of Owner: ')
+    pup_id = IntegerField("Id of Puppy: ")
+    submit = SubmitField('Add Owner')
+
+
+# class DelForm(FlaskForm):
+#     id = IntegerField("Id Number of Puppy to Remove: ")
+#     submit = SubmitField("Remove Puppy")
